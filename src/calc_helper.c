df67d25c9dd6a5f2b4faa7f0e46e34d7814f56d7
Bartek Sadlej <barteksaadlej@gmail.com>
2021-04-27 16:37:18 +0200

    try different way to read poly

--- src/calc_helper.c
+++
@@ -1,5 +0,0 @@
-#include "calc_helper.h"
-
-void read_poly(Stack stack);
-
-void read_order(char c);
--- src/calc_helper.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "stack.h"
-
-void read_poly(Stack stack);
-
-void read_order(char c);
